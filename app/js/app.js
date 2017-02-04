23e47366ff3ff7b6f66bcbef7f1a088350e0eacb
kartik <rkartik1992@gmail.com>
2017-02-05 03:14:59 +0530

    initial commit.

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+(function() {
+    /**
+     *  Module
+     *
+     * Description
+     */
+    console.log('first console');
+    var myApp = angular.module('myApp', []);
+    myApp.controller('myFirstController', ['$scope', function($scope) {
+        console.log('second console');
+        window.myFirstControllerScope = $scope;
+        $scope.firstname = "John";
+        $scope.lastname = "Doe";
+    }])
+}());
